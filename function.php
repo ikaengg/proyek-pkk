75b0149d6e8b97856eef57b4574bebe578ef66f0
ikaengg <114454087+ikaengg@users.noreply.github.com>
2022-09-27 12:46:33 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,42 @@
+<?php
+$data=[
+    [
+        "identitas" => "Rizkan<br>
+        17-03-2006<br>
+        Benda<br>",
+    "foto" => "rizkan.jpg"
+    ],
+    [
+        "identitas" => "Kelana<br>
+        04-09-2005<br>
+        Kalangsari<br>",
+    "foto" => "kelana.jpg"
+    ],
+    [
+        "identitas" => "Gilang<br>
+        12-10-2005<br>
+        Babakan New<br>",
+    "foto" => "gilang.jpg"
+    ]
+    ];
+    $produk=[
+    [
+        "nama_p" => "chesse cake",
+     "harga" => "5K",
+    "stock" => "25",
+    "foto" => "cc.jpg"
+    ],
+    [
+        "nama_p" => "kornet sapi",
+     "harga" => "2K",
+    "stock" => "20",
+    "foto" => "kor.jpeg"
+    ],
+    [
+        "nama_p" => "risol mayo",
+     "harga" => "2,5K",
+    "stock" => "15",
+    "foto" => "rm.jpeg"
+    ]
+];
+?>
